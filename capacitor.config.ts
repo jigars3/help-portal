94491629ca8e4351b7afebfedabdfc7136c531cd
jigar.pandya <jigar.pandya@ptp.media>
2023-08-20 19:00:08 -0400

    modified config cap

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'help',
-  webDir: '../../dist/apps/help',
+ // webDir: '../../dist/apps/help',
   bundledWebRuntime: false,
 };
 
